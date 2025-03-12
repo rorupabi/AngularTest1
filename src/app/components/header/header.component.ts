33559e7228393fc37895d1062c568dc66b770820
Ro Padilla <157687882+rorupabi@users.noreply.github.com>
2025-03-12 23:05:17 +0800

    PR

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -6,15 +6,13 @@ import { PrimaryButtonComponent } from '../primary-button/primary-button.compone
   imports: [PrimaryButtonComponent],
   template: `
   <div class="bg-slate-100 px-4 py-3 shadow-md flex justify-between items-center">
-    <span>TONO 21</span>
-    <app-primary-button label="Cart" (btnClicked)="showButtonClicked()"/>
+    <span class="text-xl semi-fontbold">TONO 21</span>
+    <app-primary-button label="Cart"/>
   </div>`,
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
-  showButtonClicked() {
-    console.log('clicked')
-  }
+
 
 }
--- src/app/components/primary-button/primary-button.component.ts
+++ src/app/components/primary-button/primary-button.component.ts
@@ -4,18 +4,14 @@ import { Component, input, output } from '@angular/core';
   selector: 'app-primary-button',
   imports: [],
   template: `
-  <button (click)="btnClicked.emit()">
-    Cart
+  <button class="bg-blue-500 rounded shadow-md px-4 py-2 w-full text-white hover:bg-blue-700 duration-300" (click)="btnClicked.emit()">
+    <span class="text-md">{{ label()}}</span>
   </button>`,
   styleUrl: './primary-button.component.scss'
 })
 export class PrimaryButtonComponent {
 
-  handleBtnClick() {
-    this.btnClicked.emit();
-  }
-
   btnClicked = output();
 
-  label = input('')
+  label = input<string>()
 }
